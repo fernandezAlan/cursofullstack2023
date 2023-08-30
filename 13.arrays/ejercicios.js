f013b19bbfbf79b9e7ea0dc1ef05453cf734c1d6
alan <alan.fer09@gmail.com>
2023-08-29 22:23:23 -0300

    add ejercicio

---
+++ 13.arrays/ejercicios.js
@@ -0,0 +1,9 @@
+/*
+1- crear una función que tome por parámetro un array con números y devuelva UN NUEVO array con todos los números del primer array multiplicados por dos.
+
+2- crear una función que tome como parámetro un arreglo de strings y devuelva UN NUEVO arreglo con todos los strings del primero pero en mayúsculas.
+
+3 crear una función que tome como parámetro un arreglo de números y devuelva UN NUEVO arreglo con solamente los números del primer arreglo que sean mayores a 10 (un filtro)
+por ejemplo:
+ si la función recibe este arreglo :[3,15,8,22,34,2] debe devolver este arreglo ===> [15,22,34] 
+*/
