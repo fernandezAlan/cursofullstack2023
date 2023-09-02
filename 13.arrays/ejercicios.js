8d26364613726c5478c449c5f5a28f479062dc77
alan <alan.fer09@gmail.com>
2023-09-01 21:38:33 -0300

    add arrays folder

--- 13.arrays/ejercicios.js
+++ 13.arrays/ejercicios.js
@@ -7,3 +7,177 @@
 por ejemplo:
  si la función recibe este arreglo :[3,15,8,22,34,2] debe devolver este arreglo ===> [15,22,34] 
 */
+
+//declarando
+function multiplicar(arr){
+var newArr = []
+for(var i=0;i<arr.length;i++){
+   var res =  arr[i] * 2 //4-6-8
+   newArr.push(res) //[4,6,8]
+}
+return newArr
+}
+
+// ejecutar
+var example = [2,3,4,5,6]
+var arr2 = multiplicar(example)
+var arr3 = multiplicar([10,20,30,40])
+//console.log("arr2",arr2)
+//console.log("arr3",arr3)
+
+function mayusculas(arr){
+    var newArr=[]
+    for(var i=0;i<arr.length;i++){
+       var res =  arr[i].toUpperCase()
+       newArr.push(res)
+    }
+
+    return newArr
+}
+
+var may = mayusculas(["alan","hola","argentina"])
+//console.log("may",may)
+
+
+function map(arr,fn){
+    var newArr=[]
+    for(var i=0;i<arr.length;i++){
+        var res = fn(arr[i])
+        newArr.push(res)
+    }
+    return newArr
+}
+
+var map1 = map([2,3,4,5,6],function(el){
+    return el * 2
+})
+
+var map2 = map([10,20,30],function(el){
+    return el * 3
+})
+
+
+var map3 = map(["a","b","c"],function(el){
+    return el.toUpperCase()
+})
+
+//console.log("map1",map1)
+//console.log("map2",map2)
+//console.log("map3",map3)
+
+
+var map1 = map([2,3,4,5,6], el => el * 2)
+var map2 = map([10,20,30], el => el * 3)
+var map3 = map(["a","b","c"], el => el.toUpperCase())
+
+
+
+var fn = function(){
+
+}
+// arrow function/ funciones flechas
+var fn2 = ()=>{
+
+}
+
+
+
+
+function sumar(a,b){
+    return a + b
+}
+// funcion flecha
+var sumar = (a,b)=>{
+    return a + b
+}
+
+var sumar = (a,b)=> a + b
+
+
+function mayuscula(string){
+    return string.toUpperCase()
+}
+
+var mayuscula = (string)=>{
+    return string.toUpperCase()
+}
+
+var mayuscula = (string)=>string.toUpperCase()
+
+var mayuscula = string => string.toUpperCase()
+
+
+var arr = [1,2,3,4,5]
+var res = arr.map((el)=>el * 2)
+//console.log("res",res)
+
+arr.forEach(function(el){
+    //console.log("el",el)
+})
+
+
+//FILTRO
+
+function filtro(arr){
+    var newArr=[]
+    for(var i=0;i < arr.length;i++){
+        if(arr[i]>=10){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+function filtro (arr,fn){
+    var newArr=[]
+    for(var i=0;i < arr.length;i++){
+        var cond = fn(arr[i]) // true-false
+        if(cond){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+var res = filtro([3,15,8,22,34,2],function(el){
+    return el %2===0
+})
+//console.log("res",res)
+
+var arr = [1,2,3,4,5]
+var pares = arr.filter(function(el){
+    return el %2===0
+})
+
+console.log("pares",pares)
+
+
+
+// van a crear una funcion que se llame cerosAlFinal, recibir un arreglo con elementos por parametro por ejemplo: 
+// [10,0,"a",true,0,15,false,"b","c"]
+// tiene que devolver un nuevo arreglo, con todos los ceros del arreglo original al final del nuevo arreglo
+
+//[10,0,"a",true,0,15,false,"b","c"] ===>[10,"a",true,15,false,"b","c",0,0]
+//[0,0,0,12,55,"a",0,false] ==== > [12,55,"a",false,0,0,0,0]
+
+
+//[15,true,"a",10,0,0]
+
+function cerosAlFinal(arr){
+    var newArr=[]
+    for(var i=0;i< arr.length;i++){
+        if(arr[i] === 0){
+            newArr.push(arr[i])
+        }else{
+            newArr.unshift(arr[i])
+        }
+    }
+    return newArr
+}
+
+function cerosAlFinal(arr){
+    var ceros = arr.filter(el=>el===0) //[0,0,0,0]
+    var noCeros = arr.filter(el=>el!==0)//[1,2,3,4,5]
+    var newArr = noCeros.concat(ceros) //[1,2,3,4,5,0,0,0,0]
+    return newArr
+}
---
+++ 14.arrays_2/ejercicios.js
@@ -0,0 +1,42 @@
+/*
+1- crear una funcion llamada reverseString que tome como parametro un string y devuelva el string alreves
+por ejemplo la funcion puede recibir el siguiente string "hola" y deberia devolver el string "aloh"
+tomen en cuenta que se puede iterar un string de la misma manera que iteramos un for para acceder a cada caracter
+pueden leer el siguiente articulo para saber como iterar un string y trabjar con cada uno de sus caracteres:
+https://buzzcoder.gitbooks.io/codecraft-javascript/content/string/loop-through-a-string.html
+
+
+
+2- crear una funcion llamada filtroDeStrings la funcion recibe un arreglo con varios tipos de datos y debe devolver un nuevo arreglo pero solamente con los strings 
+ej:
+recibimos el siguiente arreglo por parametro ====> [12,false,"b",10,"a","hola",24] 
+y la funcion debe devolver =====> ["b","a","hola"] 
+
+
+3- Manejar dos arreglos
+Debés crear una función llamada `arrayHandler` que reciba dos arreglos de igual largo como parámetros y muestre en la consola “Soy {elemento de array 1} y yo soy {elemento de array 2}"".
+
+Ejemplo:
+arrayHandler([1,2,3,4], ["h","o","l","a"]) debe mostrar: 
+		Soy 1 y yo soy h
+        Soy 2 y yo soy o
+        Soy 3 y yo soy l
+        Soy 4 y yo soy a
+
+
+4-Mezclar arreglos
+Debés crear una función llamada `mezclarArreglos` que reciba dos arreglos como parámetros y devuelva uno nuevo con los elementos de ambos, de 1 en 1.
+
+Ejemplo: 
+mezclarArreglos([1,2,3,4],["h","o","l","a"]) debe retornar [1, "h", 2,"o", 3,"l",4,"a"]
+mezclarArreglos([1,2,3,4], ["h","p"]) debe retornar [1, "h", 2, "p", 3 , 4]
+mezclarArreglos(["h","p"], [1,2,3,4]) debe retornar ["h", 1, "p", 2, 3 , 4]
+
+5-Mínima Suma
+Debés crear una función llamada `minSum` que reciba un arreglo de números desordenados  y devuelva la suma entre los dos números más chicos.
+
+Ejemplo: 
+minSum([7, 6, 5, 4, 3, 2, 1]) debe retornar 3 (2 + 1)
+minSum([1, 10, 43, 900, 20, 8]) debe retornar 9
+
+*/
